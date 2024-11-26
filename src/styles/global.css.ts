76cacdd12176de1bf190e64c267b65795e7508db
Teemukissa <tremppu@hotmail.com>
2024-11-27 00:56:53 +0200

    fix: better code visibility

--- src/styles/global.css.ts
+++ src/styles/global.css.ts
@@ -8,4 +8,14 @@ export const globalStyles: CssStyles = {
   a: {
     color: "currentcolor",
   },
+  code: {
+    background: "{theme.altBackground}",
+  },
+  ":where(p,li) > code": {
+    padding: "1px 2px",
+  },
+  "pre:has(code)": {
+    background: "{theme.altBackground}",
+    padding: "16px",
+  },
 };
