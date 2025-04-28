49494982b77b56ba58abf513086b0c232b4ba5bd
Teemukissa <tremppu@hotmail.com>
2025-04-28 22:40:28 +0300

    fix: better margings for markdown

--- src/app/docs/[slug]/doc-page.css.ts
+++ src/app/docs/[slug]/doc-page.css.ts
@@ -4,14 +4,29 @@ import { HDClamp } from "../../../styles/helpers.css";
 export const DocPageWrapper = styled("div", {
   base: {
     padding: "0 {spacing.large} {spacing.large}",
-    display: "flex",
-    flexDirection: "column",
-    gap: "{spacing.medium}",
     lineHeight: "1.5em",
     fontStyle: "initial",
     maxWidth: HDClamp(960),
     "@smallDesktopDown": {
       padding: 0,
+    },
+    "h1:has(+ .heading)": {
+      marginBottom: "1em",
+    },
+    ".heading": {
+      ":not(:first-child)": {
+        marginTop: "1.5em",
+      },
+      marginBottom: "0.5em",
+    },
+    p: {
+      marginBlock: "1em",
+    },
+    pre: {
+      marginBlock: "2em",
+    },
+    "*:is(p,pre,ul,ol) + h3": {
+      marginTop: "1.5em",
     },
   },
 });
