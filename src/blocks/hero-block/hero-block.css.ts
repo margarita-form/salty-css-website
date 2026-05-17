91e595844dc66880a2981eea4ea6d3eab937382c
Teemukissa <tremppu@hotmail.com>
2026-05-17 13:00:01 +0300

    fix: remove hero section bg

--- src/blocks/hero-block/hero-block.css.ts
+++ src/blocks/hero-block/hero-block.css.ts
@@ -4,7 +4,6 @@ export const HeroBlockWrapper = styled("section", {
   base: {
     padding: "{spacing.blockMargin} {spacing.pageMargin}",
     width: "100%",
-    background: "{theme.background}",
   },
 });
 
