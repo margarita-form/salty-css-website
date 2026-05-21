0213adb933d1d187f8a8b958adcd396850707d8d
Teemukissa <tremppu@hotmail.com>
2026-05-21 21:23:56 +0300

    fix: file & framework linking

--- src/components/dynamic-link/dynamic-link.tsx
+++ src/components/dynamic-link/dynamic-link.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import Link from "next/link";
-import { AllHTMLAttributes, createElement } from "react";
+import { AllHTMLAttributes, createElement, useCallback } from "react";
+import { useFramework } from "../../app/docs/components/framework-context";
 
 interface DynamicLinkProps extends AllHTMLAttributes<HTMLElement> {
   href?: string;
@@ -15,12 +18,36 @@ export const DynamicLink = ({
   element,
   ...rest
 }: DynamicLinkProps) => {
+  const { framework, setFramework } = useFramework();
+
+  const onClick = useCallback(
+    (e: React.MouseEvent<HTMLAnchorElement>) => {
+      const href = e.currentTarget.getAttribute("href");
+
+      const regex = /docs\/(react|next|astro)\//;
+      if (href && regex.test(href)) {
+        const match = href.match(regex);
+        if (match && match[1] && match[1] !== framework) {
+          setFramework(match[1] as "react" | "next" | "astro");
+        }
+      }
+    },
+    [framework, setFramework],
+  );
+
   if (!href)
     return createElement(element || "span", { className, ...rest }, children);
 
-  const target = href.startsWith("http") ? "_blank" : undefined;
+  const target =
+    href.startsWith("http") || /.+\.\w+$/.test(href) ? "_blank" : undefined;
   return (
-    <Link href={href} target={target} className={className} {...rest}>
+    <Link
+      href={href}
+      target={target}
+      className={className}
+      onClick={onClick}
+      {...rest}
+    >
       {children}
     </Link>
   );
